45c08412ca938dcf2de3da83462559ee17ee5d20
AbbasovaNigar <abbasovanigarrr@gmail.com>
2026-09-24 22:21:51 +0400

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,9 @@
+public class Task4 {
+    public static void main(String[] args){
+        float selsi = 23.2f;
+        float faranhate = (selsi * 9) / 5 + 32;
+         //°F = °C*9/5+32
+        System.out.print(faranhate);
+        System.out.print("F");
+}
+}
